fd07dd9a6c00f481ac1f693bc4f821eea5e2872f
Yusuf  Ajetunmobi <Yusuf Ajetunmobi>
2024-05-12 19:17:54 +0100

    my first ASP.NET commit

---
+++ Data/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿namespace book_store_app.Data.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public String Descripion { get; set; }
+        public bool? IsRead { get; set; }
+        public DateTime DateRead { get; set; }
+        public int? Rate { get; set; }
+        public String Genre { get; set; }
+        public String Author { get; set; }
+        public String CoverPicUrl { get; set; }
+        public DateTime DateAdded { get; set; }
+    }
+}
